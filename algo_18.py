829e9aaf84a2a5dc04ce14683f62a46ad79d05d7
arslon <a4317990a>
2021-01-16 20:25:05 +0500

    18 solved

--- algo_18.py
+++ algo_18.py
@@ -1,10 +1,16 @@
-import math
+import math as mt
 
 input_data = input().split()
+
 x = float(input_data[0])
 y = float(input_data[1])
 
-surat = (1/x+(2/x**2)+(3/x**3))+math.e**((x**2)+3*x)
-max_raj = math.atan(x+y)+abs(5+x)**2
-ans = surat/max_raj - math.cos((y**2)+(x**2)/2)**2
+top = 1/(x+2/x**2 +3/x**3) +  mt.pow(mt.e,x**2 +3*x)
+
+bottom = mt.atan(x+y) + mt.fabs(5 + x)**2
+
+right = mt.cos(y**2 + x**2 / 2) **2
+
+ans = top / bottom - right
+
 print("{:.2f}".format(ans))
