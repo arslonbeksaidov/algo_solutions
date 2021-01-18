66455c1136b3224b49953062c79d1cb263126278
arslon <a4317990a>
2021-01-18 14:19:54 +0500

    19 solved

--- algo_19.py
+++ algo_19.py
@@ -1,7 +1,9 @@
 import math
 input_data = input().split()
-x = float(input_data[0])
-y = float(input_data[1])
+x = int(input_data[0])
+y = int(input_data[1])
 
-z = math.log(abs(math.pow(x+y, 2) + math.pow(abs(y)+2, 0.5) - (x-x*y/(math.pow(x, 2) / 2 - 5)))) + math.cos(x+y)**2 /(x+y)**1/3
-print("{:.2f}".format(z))
+ans = math.log(abs(math.pow(x+y, 2) + math.sqrt(abs(y)+2,) - (x - (x*y / ((x**2 / 2) - 5))))) + \
+      math.cos(x + y)**2 / (x + y)**(1/3)
+
+print("{:.2f}".format(ans))
